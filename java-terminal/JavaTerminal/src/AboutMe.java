698c6515bd75d9bef7214c055e7188351dc3754a
Eloiza <eloizaams@yahoo.com.br>
2024-07-30 08:43:24 -0300

    feat: add project JavaTerminal

---
+++ java-terminal/JavaTerminal/src/AboutMe.java
@@ -0,0 +1,11 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String nome = args[0];
+        String sobrenome = args[1];
+        int idade = Integer.valueOf(args[2]);
+        System.out.println("Me chamo "+nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos.");
+        int num1 = 1;
+        
+    }
+}
